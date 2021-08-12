ea4a7dc4512425e47d93ab7af65f6cdd3bd0f38f
Rafael Lopez <lopez.rafael08@gmail.com>
2021-08-12 15:27:38 -0400

    CARDS: Functions and Return Types

--- Cards/main.go
+++ Cards/main.go
@@ -3,17 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// Two ways to declare a new variable
-
-	// 1) Typical way
-	// var card string = "Ace of Spades"
-
-	// 2) Go infers from the right side what data type to use
-	card := "Ace of Spades"
+	card := newCard()
 
 	fmt.Println(card)
+}
 
-	// Assign a new value to existing variables
-	card = "New Value"
-	fmt.Println(card)
+func newCard() string {
+	return "Five of Diamonds"
 }
