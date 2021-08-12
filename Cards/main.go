5a4a1b0680c0b6e9bab16eb2e6db9cdca6c074be
Rafael Lopez <lopez.rafael08@gmail.com>
2021-08-12 15:56:54 -0400

    CARDS: Slices and For loops

--- Cards/main.go
+++ Cards/main.go
@@ -3,9 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	card := newCard()
+	// Delcare a Slice
+	cards := []string{"Ace of Diamonds", newCard()}
+	// Add values to the slice. The append() function doesn't modify the original slice, that's why we are assigning the result to cards
+	cards = append(cards, "Six of Spades")
 
-	fmt.Println(card)
+	// Iterate through the slice
+	// When ranging over a slice, two values are returned for each iteration.
+	// The first is the index, and the second is a copy of the element at that index.
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
