960667eb116c0e8554ba50e1c094d53218d2744f
Rafael Lopez <lopez.rafael08@gmail.com>
2021-08-12 16:38:34 -0400

    CARDS: Receiver Functions

--- Cards/main.go
+++ Cards/main.go
@@ -1,19 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	// Delcare a Slice
-	cards := []string{"Ace of Diamonds", newCard()}
-	// Add values to the slice. The append() function doesn't modify the original slice, that's why we are assigning the result to cards
+	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	// Iterate through the slice
-	// When ranging over a slice, two values are returned for each iteration.
-	// The first is the index, and the second is a copy of the element at that index.
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
