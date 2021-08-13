977ad50b82c6f9444227f77890c2b037e08292bd
Rafael Lopez <lopez.rafael08@gmail.com>
2021-08-13 15:29:09 -0400

    Structs: Declare a struct

--- Structs/main.go
+++ Structs/main.go
@@ -1,10 +1,21 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
 }
 
 func main() {
+	// There are multiple ways to initialize a struct
 
+	// a)
+	// No need to specify the properties, Go assumes the order based on the definition of the struct
+	//alex := person{"Alex", "Anderson"}
+
+	// b)
+	alex := person{firstName: "Alex", lastName: "Anderson"}
+
+	fmt.Println(alex)
 }
