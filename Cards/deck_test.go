6df3deb83bbf5b3336342757e3e34d19a2b642f0
Rafael Lopez <lopez.rafael08@gmail.com>
2021-08-13 14:30:00 -0400

    CARDS: Test File IO

--- Cards/deck_test.go
+++ Cards/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 // Had to run "go mod init Cards" at ~/Documents/GitHub/Go/Cards to be able to run tests
 func TestNewDeck(t *testing.T) {
@@ -17,4 +20,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Four of Clubs" {
 		t.Errorf("Expected first card of Four of Clubs, but got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
