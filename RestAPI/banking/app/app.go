bfd305ca0191202620e7c71c1a1481fa44cb1f68
Rafael Lopez <lopez.rafael08@gmail.com>
2021-08-15 13:05:03 -0400

    Rest API - Router Basics (Banking): Add gorialla/mux + Handle path variable + Add POST endpoint

--- RestAPI/banking/app/app.go
+++ RestAPI/banking/app/app.go
@@ -1,20 +1,37 @@
 package app
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Start() {
 	// Delcare our own Multiplexer
-	mux := http.NewServeMux()
+	//mux := http.NewServeMux()
+	router := mux.NewRouter()
 
 	// Define routes
 	// - Register the '/greet' endpoint with the Default Multiplexer
-	mux.HandleFunc("/greet", greet)
-	mux.HandleFunc("/customers", getAllCustomers)
+	router.HandleFunc("/greet", greet).Methods(http.MethodGet)
+	router.HandleFunc("/customers", getAllCustomers).Methods(http.MethodGet)
+	// router.HandleFunc("/customers/{customer_id}", getCustomer).Methods(http.MethodGet)
+	router.HandleFunc("/customers/{customer_id:[0-9]+}", getCustomer).Methods(http.MethodGet)
+	router.HandleFunc("/customers", createCustomer).Methods(http.MethodPost)
 
 	// To specify where to start the server (machine, port, etc)
 	// We wrap ListenAndServe in log.Fatal in case the server fails to start
-	log.Fatal(http.ListenAndServe("localhost:8000", mux))
+	log.Fatal(http.ListenAndServe("localhost:8000", router))
+}
+
+func getCustomer(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+
+	fmt.Fprint(w, vars["customer_id"])
+}
+
+func createCustomer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Post request received")
 }
--- RestAPI/banking/go.mod
+++ RestAPI/banking/go.mod
@@ -1,3 +1,5 @@
 module github.com/Rafael-Lopez/GO/RESTAPI/banking
 
 go 1.16
+
+require github.com/gorilla/mux v1.8.0 // indirect
