5ccf6deea4fdc0045f49c104c12efc06f182ff12
Rafael Lopez <lopez.rafael08@gmail.com>
2021-08-16 00:30:58 -0400

    Rest API - Banking: Add DTO package + CustomerResponse dto

--- RestAPI/banking/service/customerService.go
+++ RestAPI/banking/service/customerService.go
@@ -2,24 +2,45 @@ package service
 
 import (
 	"github.com/Rafael-Lopez/GO/RESTAPI/banking/domain"
+	"github.com/Rafael-Lopez/GO/RESTAPI/banking/dto"
 	"github.com/Rafael-Lopez/GO/RESTAPI/banking/errs"
 )
 
 type CustomerService interface {
-	GetAllCustomers() ([]domain.Customer, *errs.AppError)
-	GetCustomer(id string) (*domain.Customer, *errs.AppError)
+	GetAllCustomers() ([]dto.CustomerResponse, *errs.AppError)
+	GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError)
 }
 
 type DefaultCustomerService struct {
 	repo domain.CustomerRepository
 }
 
-func (s DefaultCustomerService) GetAllCustomers() ([]domain.Customer, *errs.AppError) {
-	return s.repo.FindAll()
+func (s DefaultCustomerService) GetAllCustomers() ([]dto.CustomerResponse, *errs.AppError) {
+	customers, err := s.repo.FindAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	response := make([]dto.CustomerResponse, 0)
+
+	for _, c := range customers {
+		response = append(response, c.ToDto())
+	}
+
+	return response, nil
 }
 
-func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, *errs.AppError) {
-	return s.repo.ById(id)
+func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError) {
+	c, err := s.repo.ById(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	response := c.ToDto()
+
+	return &response, nil
 }
 
 func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {
